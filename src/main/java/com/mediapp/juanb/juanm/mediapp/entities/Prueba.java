2834fde35754c64c24d36d7a39c71913e3f24761
JuanMartinezB <juans.martinezb@uqvirtual.edu.co>
2025-09-07 10:34:10 -0500

    Entidades

---
+++ src/main/java/com/mediapp/juanb/juanm/mediapp/entities/Prueba.java
@@ -0,0 +1,12 @@
+package com.mediapp.juanb.juanm.mediapp.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+public class Prueba {
+}
