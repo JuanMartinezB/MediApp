99f235af290ae092af398ae79078eef8e86add46
Juan David Bedoya <bedoyaj919@gmail.com>
2025-09-07 09:19:03 -0500

    Proyecto MediApp

---
+++ src/main/java/com/mediapp/juanb/juanm/mediapp/MediAppMain.java
@@ -0,0 +1,13 @@
+package com.mediapp.juanb.juanm.mediapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediAppMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediAppMain.class, args);
+	}
+
+}
---
+++ src/main/java/com/mediapp/juanb/juanm/mediapp/entities/User.java
@@ -0,0 +1,8 @@
+package com.mediapp.juanb.juanm.mediapp.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    
+}
